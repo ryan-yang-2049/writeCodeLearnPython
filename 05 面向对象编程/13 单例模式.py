e324302a8f64e7e9b47f9886c71add8423ae610e
ryan <11066986@qq.com>
2018-03-19 22:11:43 +0800

    mymeta

--- 05 面向对象编程/13 单例模式.py
+++ 05 面向对象编程/13 单例模式.py
@@ -38,55 +38,140 @@
 # print("obj2的内存地址",id(obj2))
 # print("obj3的内存地址",id(obj3))
 
-# import time
-# import threading
-# class Singleton(object):
-#     _instance_lock = threading.Lock()
+
+# 基于元类实现MySQL 单例模式
 #
-#     def __init__(self):
-#         time.sleep(1)
-#
-#     @classmethod
-#     def instance(cls, *args, **kwargs):
-#         if not hasattr(Singleton, "_instance"):
-#             with Singleton._instance_lock:
-#                 if not hasattr(Singleton, "_instance"):
-#                     Singleton._instance = Singleton(*args, **kwargs)
-#         return Singleton._instance
-#
-#
-# def task(arg):
-#     obj = Singleton.instance()
-#     print(obj)
-# for i in range(10):
-#     t = threading.Thread(target=task,args=[i,])
-#     t.start()
-# # time.sleep(20)
-# obj = Singleton.instance()
-# print("11",obj)
+class Mymeta(type):
+	def __init__(self, class_name, class_bases, class_dic):
+		if not class_name.istitle():
+			raise TypeError('类名必须大写')
+		if '__doc__' not in class_dic or not class_dic['__doc__'].strip():
+			raise TypeError('注释不能为空')
+		super(Mymeta, self).__init__(class_name, class_bases, class_dic)
+		self.__instance = None
+
+	def __call__(self, *args, **kwargs):
+		if not self.__instance:
+			obj = object.__new__(self)
+			self.__init__(obj)
+			self.__instance = obj
+		return self.__instance
 
 
-# 使用装饰器实现单例模式。
-# def Singleton(cls):
-#     _instance = {}
+class Mysql(object, metaclass=Mymeta):
+	'''
+	MySQL 连接
+	'''
+
+	def __init__(self):
+		self.host = '127.0.0.1'
+		self.port = '3306'
+
+	def conn(self):
+		print('conn')
+
+	def execute(self):
+		print('execute')
+
+
+obj1 = Mysql()
+obj2 = Mysql()
+obj3 = Mysql()
+print("obj1的内存地址",id(obj1))
+print("obj2的内存地址",id(obj2))
+print("obj3的内存地址",id(obj3))
+
+
+
+# class Mymeta(type):
+# 	def __init__(self, name, bases, dic):  # 定义类Mysql时就触发
+# 		self.__instance = None
+# 		super().__init__(name, bases, dic)
 #
-#     def _singleton(*args, **kargs):
-#         if cls not in _instance:
-#             _instance[cls] = cls(*args,**kargs)
-#         return _instance[cls]
+# 	def __call__(self, *args, **kwargs):  # Mysql(...)时触发
 #
-#     return _singleton
+# 		if not self.__instance:
+# 			self.__instance = object.__new__(self)  # 产生对象
+# 			self.__init__(self.__instance, *args, **kwargs)  # 初始化对象
+# 			# 上述两步可以合成下面一步
+# 			# self.__instance=super().__call__(*args,**kwargs)
 #
-#
-# @Singleton
-# class A(object):
-#     a = 1
-#
-#     def __init__(self, x=0):
-#         self.x = x
+# 		return self.__instance
 #
 #
-# a1 = A(2)
-# a2 = A(3)
-# print(id(a1))
-# print(id(a2))
+# class Mysql(metaclass=Mymeta):
+# 	def __init__(self, host='127.0.0.1', port='3306'):
+# 		self.host = host
+# 		self.port = port
+#
+#
+# obj1 = Mysql()
+# obj2 = Mysql()
+#
+# print(obj1 is obj2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		# import time
+		# import threading
+		# class Singleton(object):
+		#     _instance_lock = threading.Lock()
+		#
+		#     def __init__(self):
+		#         time.sleep(1)
+		#
+		#     @classmethod
+		#     def instance(cls, *args, **kwargs):
+		#         if not hasattr(Singleton, "_instance"):
+		#             with Singleton._instance_lock:
+		#                 if not hasattr(Singleton, "_instance"):
+		#                     Singleton._instance = Singleton(*args, **kwargs)
+		#         return Singleton._instance
+		#
+		#
+		# def task(arg):
+		#     obj = Singleton.instance()
+		#     print(obj)
+		# for i in range(10):
+		#     t = threading.Thread(target=task,args=[i,])
+		#     t.start()
+		# # time.sleep(20)
+		# obj = Singleton.instance()
+		# print("11",obj)
+
+
+		# 使用装饰器实现单例模式。
+		# def Singleton(cls):
+		#     _instance = {}
+		#
+		#     def _singleton(*args, **kargs):
+		#         if cls not in _instance:
+		#             _instance[cls] = cls(*args,**kargs)
+		#         return _instance[cls]
+		#
+		#     return _singleton
+		#
+		#
+		# @Singleton
+		# class A(object):
+		#     a = 1
+		#
+		#     def __init__(self, x=0):
+		#         self.x = x
+		#
+		#
+		# a1 = A(2)
+		# a2 = A(3)
+		# print(id(a1))
+		# print(id(a2))
--- 05 面向对象编程/14 python魔法方法/04 __call__方法.py
+++ 05 面向对象编程/14 python魔法方法/04 __call__方法.py
@@ -7,22 +7,31 @@
 
 # __call__(self,  *args, **kwargs) 如果类实现了这个方法，相当于把这个类型的对象当作函数来使用，相当于 重载了括号运算符
 
+# 让对象变成一个可调用对象
 # class Person(object):
 #
-#     def __init__(self):
-#         print("init")
+# 	def __init__(self):
+# 		print("init")
 #
 #
-#     def __call__(self, *args, **kwargs):
-#         print("i am chinese")
+# 	def __call__(self, *args, **kwargs):
+# 		print("i am chinese")
+# 		print(self)         #<__main__.Person object at 0x10fa55390>
+# 		print(args)         # (1, 2, 3) 位置参数
+# 		print(kwargs)       # {'a': 1, 'b': 2, 'c': 3} 关键字参数
 #
+#
+# 	def tell_info(self):
+# 		print("aaa")
 #
 # ryan = Person()
 #
-# ryan()
+# ryan(1,2,3,a=1,b=2,c=3)
 
 
+# 元类内部也应有一个 __call__ 方法，会在调用Foo时触发执行。
 
+#
 class Mymeta(type):
     def __init__(self,class_name,class_bases,class_dic):
         if not class_name.istitle():
@@ -33,17 +42,18 @@ class Mymeta(type):
 
         super(Mymeta,self).__init__(class_name,class_bases,class_dic)
 
-    # def __call__(self, *args, **kwargs): #obj=Chinese('egon',age=18)
-    #     # print(self) #self=Chinese
-    #     # print(args) #args=('egon',)
-    #     # print(kwargs) #kwargs={'age': 18}
-    #
-    #     #第一件事：先造一个空对象obj
-    #     obj=object.__new__(self)
-    #     #第二件事：初始化obj
-    #     self.__init__(obj,*args,**kwargs)
-    #     #第三件事：返回obj
-    #     return obj
+    def __call__(self, *args, **kwargs): #obj=Chinese('ryan',age=18)
+        # print(self) #self=Chinese
+        # print(args) #args=('ryan',)
+        # print(kwargs) #kwargs={'age': 18}
+
+        #第一件事：先造一个空对象obj
+        obj=object.__new__(self)
+        #第二件事：初始化obj
+        self.__init__(obj,*args,**kwargs)
+        # 这里可以做一些控制实例化对象的方法
+        #第三件事：返回obj
+        return obj
 
 class Chinese(object,metaclass=Mymeta):
     '''
@@ -58,7 +68,7 @@ class Chinese(object,metaclass=Mymeta):
     def talk(self):
         print('%s is talking' %self.name)
 
-obj=Chinese('ryan',age=18) #Chinese.__call__(Chinese,'egon',18)
+obj=Chinese('ryan',age=18) #Chinese.__call__(Chinese,'ryan',18)
 # obj()
 print(obj.__dict__)
 
